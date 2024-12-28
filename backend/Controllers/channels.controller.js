07fd9c934011f09b4ccd7d307315d0f6ef2697a1
AgrawalShruti0208 <shrutiagrawal155@gmail.com>
2024-12-29 01:25:37 +0530

    error resolving part 19

--- backend/Controllers/channels.controller.js
+++ backend/Controllers/channels.controller.js
@@ -1,55 +1,6 @@
 import ChannelsModel from "../Models/ChannelsModel.js";
-import notifier from 'node-notifier';
 
 
-// controller function to add Channel to the Database
-export function addChannel(req,res){
-    
-    const channel = req.body;
-    
-        ChannelsModel.create(channel)
-            .then((savedData) =>{
-                res.send(savedData);
-                notifier.notify({
-                    title: 'Success',
-                    message: 'Channel Added',
-                    icon: '🛒'
-                })
-            }).catch((err)=>{
-                res.status(500).json([{message:"Failed to Add Channel"},{Error:err}]);
-                notifier.notify({
-                    title: '🚨Request Failed',
-                    message: 'Channel Not Added',
-                icon: '🛒'
-                });
-            });
-        
-    
-                    
-    
-}
-
-
-// To display videos of the Channel
-export function getVideosFromChannel(req,res){
-    
-    ChannelsModel.findOne({ _id : req.params.id})
-    .then(async(data)=>{
-        
-        if(!data){
-            console.log("Something Went Wrong,",err.message);
-            return res.status(400).json({message:"Something Went Wrong,"+err.message});
-        }
-
-        res.send(data);
-    })
-    .catch((err)=>{
-        //catch => if not successful to fetch data, send error 
-        console.log("Error in fetching channel details: ",err.message);
-        res.status(500).json({message:"Error in fetching channel details: "+err.message});
-    });
-       
-}
 
 export async function getChannels(req,res){
     try{
--- backend/Routes/channels.routes.js
+++ backend/Routes/channels.routes.js
@@ -1,14 +1,8 @@
-import { addChannel , getVideosFromChannel, getChannels} from "../Controllers/channels.controller.js";
+import {getChannels} from "../Controllers/channels.controller.js";
 
 function ChannelRoutes(app) {
     
-    // POST method to add channel to the Database
-        app.post('/channel',addChannel);
-
-    // FETCH videos from the channel 
-        app.get('/channel/:id',getVideosFromChannel)
-
-        app.get('/channels',getChannels)
+    app.get('/channels',getChannels)
 
 }
 
